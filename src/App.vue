a81d0ad1e3abccdb0e3c0327cc338b04f18ab6ab
Romeo Coliba <romeocoliba@gmail.com>
2024-10-24 11:59:24 +0300

    Creating HomeCards Component

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import { ref, onMounted } from "vue";
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
 </script>
 
 <template>
   <Navbar></Navbar>
   <Hero></Hero>
+  <HomeCards></HomeCards>
 </template>
