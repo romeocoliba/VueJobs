a0b6fda230d8a191f597f2b7fdd5261b38c8f0b9
Romeo Coliba <romeocoliba@gmail.com>
2024-11-04 08:35:47 +0200

    Creating Many Components and Routers

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
-import { ref, onMounted } from "vue";
 import Navbar from "./components/Navbar.vue";
-import Hero from "./components/Hero.vue";
-import HomeCards from "./components/HomeCards.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <Navbar></Navbar>
-  <Hero></Hero>
-  <HomeCards></HomeCards>
+  <RouterView></RouterView>
 </template>
